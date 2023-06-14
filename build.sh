4301343a9f98b25f1f23f4025a5eec4124acb6c5
huwei.guo <huwei.guo@deptrum.com>
2023-06-14 16:29:17 +0800

    fetch and add build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+rm -rf build/
+mkdir build/
+cd build/
+
+cmake -DCMAKE_BUILD_TYPE=Debug \
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+      .. || exit 1
+
+make -j8 || exit 1
+
+./cgalTest
+
